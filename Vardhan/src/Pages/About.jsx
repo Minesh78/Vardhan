f93d0209d0eed95f13b830d1f2c5581f35e88779
Minesh Rajput <110811001+Minesh78@users.noreply.github.com>
2023-06-06 17:39:07 +0530

    about txt color changed

--- Vardhan/src/Pages/About.jsx
+++ Vardhan/src/Pages/About.jsx
@@ -4,8 +4,8 @@ const About = () => {
   return (
     <>
     <div className=' bg-[#904B55] md:h-14 h-12 '></div>
-    <div className='w-[80%] md:w-[30%] m-auto md:mt-44 mt-28 text-justify'>
-    <h2 className='text-xl text-[#707271] font-bold '>About us</h2>
+    <div className='w-[80%]  text-[#707271] md:w-[30%] m-auto md:mt-44 mt-28 text-justify'>
+    <h2 className='text-xl font-bold '>About us</h2>
     <section className='  '>
       <p className='m-3 indent-5'>
       V Vardhan Architects and Associates  is a team of architects, artists, and civil engineers celebrating earth, air, and light.   We built spaces,master plans, buildings and products with an emphasis on environmental friendly and economically sensible solutions.
